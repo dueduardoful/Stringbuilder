d2d3573e661ff8296841fb344702546858ac5892
dueduardoful <dueduardoful@gmail.com>
2020-01-09 15:52:06 -0300

    Classe Comment finalizada

--- src/entities/Comment.java
+++ src/entities/Comment.java
@@ -1,5 +1,17 @@
 package entities;
 
 public class Comment {
+	public String text;
 
+	public Comment() {
+	}
+	public Comment(String text) {
+		this.text = text;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
 }
